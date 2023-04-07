bfcb14c055b1763ef92a803de1e29c74daab3138
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-04-07 02:18:02 -0300

    adicionados novos elementos no programa

--- game.js
+++ game.js
@@ -1,3 +1,31 @@
+const mario = $('.mario').get(0);
+const pipe = $('.pipe').get(0);
 
 
-$(".mario").on("keydown", jump);
+$(document).on('keydown', function jump () {
+    mario.classList.add('jump')
+    setTimeout(() => {
+        mario.classList.remove("jump")
+    },500)
+});
+
+const loop = setInterval (() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+    console.log(marioPosition)
+
+    if (pipePosition <= 140 && pipePosition > 0 &&marioPosition < 115) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = "./images/mario-death.png"
+        mario.style.width = "130px";
+        mario.style.marginLeft = "50px";
+
+        clearInterval(loop);
+    };
+
+
+},10);
