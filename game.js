9077a3fcab39bbe217bbafd369250d3466169bb0
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-04-10 23:00:14 -0300

    changed stuff

--- game.js
+++ game.js
@@ -1,31 +1,53 @@
-const mario = $('.mario').get(0);
+const peter = $('.peter').get(0);
 const pipe = $('.pipe').get(0);
 
 
+
 $(document).on('keydown', function jump () {
-    mario.classList.add('jump')
+    peter.classList.add('jump')
     setTimeout(() => {
-        mario.classList.remove("jump")
+        peter.classList.remove("jump")
     },500)
 });
 
+function secondAudio () {
+    let audio2 = new Audio();
+    audio2.src = "./sound/peter-hurt2.mp3"
+    audio2.play();
+    audio2.loop = true;
+}
+
 const loop = setInterval (() => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
-    console.log(marioPosition)
+    const peterPosition = +window.getComputedStyle(peter).bottom.replace("px", "");
+    console.log(peterPosition)
 
-    if (pipePosition <= 140 && pipePosition > 0 &&marioPosition < 115) {
+    if (pipePosition <= 140 && pipePosition > 0 && peterPosition < 115) {
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`
 
-        mario.style.animation = "none";
-        mario.style.bottom = `${marioPosition}px`
-        mario.src = "./images/mario-death.png"
-        mario.style.width = "130px";
-        mario.style.marginLeft = "50px";
-
+        peter.style.animation = "none";
+        peter.style.bottom = `${peterPosition}px`
+        peter.src = "./images/peter-death.png"
+        peter.style.width = "150px";
+        peter.style.marginLeft = "50px";
+        audio.src = "";
+        secondAudio();
         clearInterval(loop);
     };
 
 
 },10);
+
+var audio, playbtn;
+
+function initAudioPlayer() {
+  audio = new Audio();
+  audio.src = "./sound/brasileirinho.mp3";
+  audio.play();
+}
+
+$("#play").one("click", function () {
+    initAudioPlayer();
+})
+
